0ae751142256b61cca3386c9001fe08aee1fe5c1
Daniel Hauck <hauck-daniel@web.de>
2026-03-25 19:44:17 +0100

    rerw

--- main (3).tf
+++ main (3).tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "amzon-s3-dh"
+    bucket = "test-s3-dh-lol "
     key    = "app/terraform.tfstate" # Key könnt ihr selbst bestimmen, sollte nur über die deployments einheitlich sein
     region = "us-east-1"
   }
@@ -39,7 +39,7 @@ resource "aws_security_group" "ssh" {
 resource "aws_instance" "demo" {
   ami                    = "ami-0b6c6ebed2801a5cb" # ubuntu
   instance_type          = "t2.micro"
-  key_name               = "Key_Pair"
+  key_name               = "Pair"
   vpc_security_group_ids = [aws_security_group.ssh.id]
   tags = {
     Name = "test-server-iac"
